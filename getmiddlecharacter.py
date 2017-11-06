3ff9ac0af9ce517376906983faa784cd432a5dc1
Isaiah Reed <isaiah.codes@gmail.com>
2017-11-05 21:23:44 -0600

    Added comments explaining code

--- getmiddlecharacter.py
+++ getmiddlecharacter.py
@@ -1,8 +1,14 @@
 def get_middle(s):
     numChar = len(s)
+
+# If number of letters is even, divide by 2, split from middle number
+
     if numChar % 2 == 0:
         splitEven = list(s)
         middleChar = numChar / 2
+
+# Grab index number of character in list to join back together
+
         finalMaybe = (splitEven[int(middleCharE - 1):int(middleCharE + 1)])
         final = finalMaybe[0] + finalMaybe[1]
         return final
